ddb2881bc73ff589034ad1e8b06f2e1765ce74e5
kat <kathiacid98@gmail.com>
2025-05-12 17:42:45 -0400

    calcular promedio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const students=[];
 const tableBody=document.querySelector("#studensTable tbody")
+const average=document.getElementById("average")
 
 
 document.getElementById("studentForm").addEventListener("submit", function(e){
@@ -10,10 +11,11 @@ document.getElementById("studentForm").addEventListener("submit", function(e){
     const grade = parseFloat(document.getElementById("grade").value);
     const date=document.getElementById("date").value.trim();
 
+
     if(!name || !lastName || isNaN(grade) || grade<1 || grade >7){
         alert("Error al ingresar la Datos")
         return
-    }
+    } 
     const student ={
         name,
         lastName,
@@ -23,6 +25,7 @@ document.getElementById("studentForm").addEventListener("submit", function(e){
 students.push(student);
 //console.log(students)
 addStudentToTable(student)
+calculateAverage();
 this.reset()
 //holi
 });
@@ -30,9 +33,24 @@ function addStudentToTable(student){
     const row=document.createElement("tr")
     row.innerHTML=`
     <td>${student.name}</td>
+
+
+
+
     <td>${student.lastName}</td>
     <td>${student.date}</td>
     <td>${student.grade}</td>`;
 
     tableBody.appendChild(row);
+}
+
+function calculateAverage(){
+    if(students.length===0){
+        average.textContent="Promedio General del curso: N/A"
+        return
+    } 
+    const total=students.reduce((sum,student)=>sum+student.grade,0);
+    const prom=total/students.length;
+    average.textContent="Promedio General del curso : "+prom.toFixed(2);
+    
 }
