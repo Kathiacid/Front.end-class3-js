dbf6710255cdec7291b28df1b30e40f26b0c22c7
kat <kathiacid98@gmail.com>
2025-05-19 17:48:52 -0400

    funciones elimianr, editar

--- script.js
+++ script.js
@@ -41,20 +41,50 @@ row.innerHTML = `
     <td>${student.lastName}</td>
     <td>${student.grade}</td>
     <td>${student.date}</td>
+    <td><button class="delete-btn">Eliminar</button></td>
+    <td><button class="edit-btn">Editar</button></td>
 `;
 
     tableBody.appendChild(row);
+//-----------------------------eliminar----------------------//
+
+    row.querySelector(".delete-btn").addEventListener("click",function(){
+        deleteEstudiante(student,row);        
+
+    })
+
+function deleteEstudiante(student,row){
+    const index=students.indexOf(student);
+    if (index>-1){
+        students.splice(index,1);
+        calculateAverage();
+        row.remove();
+    }
 }
+}
+
+//-------------------------Editar---------------------------//
+
 //-------------------------average-------------------------//
-function calculateAverage(){
-    if(students.length===0){
-        average.textContent="Promedio de Calificaciones : No Disponible";
-        return
-    } 
-    const total=students.reduce((sum,student)=>sum+student.grade,0);
-    const prom=total/students.length;
-    averageText.textContent="Promedio de Calificaciones : "+prom.toFixed(2);
-    
+function calculateAverage() {
+    if (students.length === 0) {
+        averageText.textContent = "Promedio de Calificaciones : No Disponible";
+        return;
+    }
+
+    const total = students.reduce((sum, student) => sum + student.grade, 0);
+    const prom = total / students.length;
+
+    averageText.textContent = "Promedio de Calificaciones : " + prom.toFixed(2);
+
+    // agregar estilos a las cosas que se vayan generando por java script
+    averageText.style.backgroundColor = "#f999a9";
+    averageText.style.fontSize = "15px";
+    averageText.style.borderRadius = "10px";
+    averageText.style.padding = "5px";
+    averageText.style.height = "auto"; 
+    averageText.style.width = "300px";
+    averageText.style.fontFamily = '"Jua", sans-serif';
 }
 
 //-------------------------mensaje de error-------------------------//
